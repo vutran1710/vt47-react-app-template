efa958c6fa151967d857f2db9d8873814ba9b3d8
vutran1710 <me@vutr.io>
2018-07-18 17:03:38 +0700

    Update eslintrc and request handler

--- app/manage/request.js
+++ app/manage/request.js
@@ -3,16 +3,24 @@ const api = {
   production: '...some path'
 }
 
-const request = (uri, method, body) => fetch(`${api[process.env.NODE_ENV]}${uri}`, {
-  method,
-  body: body && JSON.stringify(body),
-  mode: 'same-origin',
-  headers: new Headers({
-    Accept: 'application/json',
-    'Access-Token': localStorage['casec-token'] || '',
-    'Content-Type': 'application/json'
+// If need more, checkout wretch(https://github.com/elbywan/wretch)
+// which is a very complete tool for handling async requests
+const request = (uri, method, body) => {
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`
   })
-}).then(res => res.json())
 
-export default request
+  const configs = {
+    method,
+    body: body && JSON.stringify(body),
+    headers
+  }
 
+  // Given a returned json always has an unifying "ok" key
+  const middleware = resp => resp.json().then(json => json.ok ? json : Promise.reject(json))
+
+  return fetch(`${api[process.env.NODE_ENV]}${uri}`, configs).then(middleware)
+}
+
+export { request, api }
